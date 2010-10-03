066aaf48fd7c7b89c676094060871e24d75f3d78
Ted Tibbetts <intuited@gmail.com>
2010-10-03 03:53:36 -0300

    Reworked to be more readily parametrizable.  All tests passed.

--- labelscrape.py
+++ labelscrape.py
@@ -1,76 +1,100 @@
 """Uses the lxml package to scrape data from the .odt labels file."""
 import argparse
 from lxml import etree
+from functools import partial
+
+_frame_xpath = 'draw:frame'
 
 def draw_frames(root):
-    return root.xpath('//draw:frame', namespaces=root.nsmap)
+    return root.xpath('.//' + _frame_xpath, namespaces=root.nsmap)
 
-def product_names(root, with_text=True):
+
+_xpath_text_additions = {'yes': '[text()]',
+                         'no': '[not text()]',
+                         'either': ''}
+
+_style_names = {'name': 'P10',
+                'price': 'P9'}
+
+
+def _field_filter(element, value, results):
+    """Filter an xpath result set for the absence or presence of a name."""
+
+
+def style_name_xpath(style_name):
+    return 'text:p[@text:style-name="{0}"]'.format(style_name)
+
+_field_names = ('name', 'price')
+_field_xpaths = dict((name, style_name_xpath(_style_names[name]))
+                     for name in _field_names)
+
+
+def _yes_no_text(require_text, xpath):
+    """Modify an xpath to filter or not for the presence of text."""
+    return xpath + _xpath_text_additions[require_text]
+
+
+
+def descendant_fields(field, with_text, root):
+    text_filter = partial(_yes_no_text, with_text)
+    xpath = './/' + text_filter(_field_xpaths[field])
+    return root.xpath(xpath, namespaces=root.nsmap)
+
+
+def product_names(root, with_text='yes'):
     """Return product names under the root.
 
-    If with_text is untrue, return even those product names which have no text.
+    ``text_filter`` is applied to the xpath.
     """
-    xpath = '//text:p[@text:style-name="P10"]'
-    if with_text:
-        xpath += '[text()]'
-    return root.xpath(xpath, namespaces=root.nsmap)
+    return descendant_fields('name', with_text, root)
 
-def product_prices(root, with_text=True):
+def product_prices(root, with_text='yes'):
     """Return product prices under the root.
 
-    If with_text is untrue, return even those product prices which have no text.
+    ``text_filter`` is applied to the xpath.
     """
-    xpath = '//text:p[@text:style-name="P9"]'
-    if with_text:
-        xpath += '[text()]'
+    return descendant_fields('price', with_text, root)
+
+
+def field_predicate_xpath(with_text_seq=('yes', 'yes')):
+    conditions = [_yes_no_text(wt, _field_xpaths[field])
+                  for wt, field in zip(with_text_seq, _field_names)]
+    return './/{0} and .//{1}'.format(*conditions)
+
+def draw_frames_with_names_and_prices(root, with_text=('yes', 'yes')):
+    predicate = field_predicate_xpath(with_text)
+
+    xpath = './/{0}[{1}]'.format(_frame_xpath, predicate)
+
     return root.xpath(xpath, namespaces=root.nsmap)
 
-sample_page = """\
-    <draw:frame draw:style-name="fr1" draw:name="Frame58" text:anchor-type="page" text:anchor-page-number="115" svg:x="0.799cm"
-    svg:y="4.542cm" svg:width="8.52cm" draw:z-index="57">
-      <draw:text-box fo:min-height="3.692cm">
-        <table:table table:name="Table58" table:style-name="Table58">
-          <table:table-column table:style-name="Table58.A" />
-          <table:table-column table:style-name="Table58.B" />
-          <table:table-column table:style-name="Table58.C" />
-          <table:table-row table:style-name="Table58.1">
-            <table:table-cell table:style-name="Table58.A1" table:number-columns-spanned="3" office:value-type="string">
-              <text:p text:style-name="P3">2385 Agricola St., Halifax, Nova Scotia 
-              <text:s text:c="32" />(902) 446-3301</text:p>
-              <text:p text:style-name="P4">The Grainery Food Co-op</text:p>
-              <text:p text:style-name="P3">also open Saturdays at the Halifax Farmers Market 1496 Lower Water Street</text:p>
-            </table:table-cell>
-            <table:covered-table-cell />
-            <table:covered-table-cell />
-          </table:table-row>
-          <table:table-row table:style-name="Table58.2">
-            <table:table-cell table:style-name="Table58.A2" table:number-columns-spanned="3" office:value-type="string">
-              <text:p text:style-name="P5">Speerville Maritimes Organic 
-              <text:s />Steel-Cut</text:p>
-              <text:p text:style-name="P10">Scottish Oatmeal</text:p>
-              <text:p text:style-name="P7">ABOUT SCOTTISH OATMEAL: Groats are stoneground to give a fine textured meal. Eaten as a
-              breakfast cereal, use 1/2 cup cereal to 1 cup water, bring to a boil and simmer until desired consistency. 
-              <text:s />Also used in many traditional Scottish baked goods.</text:p>
-            </table:table-cell>
-            <table:covered-table-cell />
-            <table:covered-table-cell />
-          </table:table-row>
-          <table:table-row table:style-name="Table58.3">
-            <table:table-cell table:style-name="Table58.A1" office:value-type="string">
-              <text:p text:style-name="P2">750g</text:p>
-            </table:table-cell>
-            <table:table-cell table:style-name="Table58.A1" office:value-type="string">
-              <text:p text:style-name="P15">
-                <text:s />
-              </text:p>
-              <text:p text:style-name="P16">http://thegrainery.ca</text:p>
-            </table:table-cell>
-            <table:table-cell table:style-name="Table58.C3" office:value-type="currency" office:currency="USD" office:value="3">
-              <text:p text:style-name="P9">$3.00</text:p>
-            </table:table-cell>
-          </table:table-row>
-        </table:table>
-        <text:p text:style-name="P1" />
-      </draw:text-box>
-    </draw:frame>
-    """
+
+
+##__  dragons
+
+def _first_or_none(seq):
+    return seq[0] if len(seq) else None
+
+def draw_frame_data(draw_frame):
+    return {'name': _first_or_none(product_names(draw_frame)),
+            'prices': _first_or_none(product_prices(draw_frame))}
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Scrape data from the grainery's .odt-format label database.",
+        )
+
+    parser.add_argument(
+        'content_file', 
+        help="The XML file to scrape.\n"
+             "Normally this will be found as the top-level file ``content.xml`` "
+             "after unzipping the .odt file."
+        )
+
+    parser.add_argument(
+        '--name',
+        help='Enable/disable filtering on the presence of a name field.'
+        )
+
+    ns = parser.parse()
--- test/test_labelscrape.py
+++ test/test_labelscrape.py
@@ -9,6 +9,12 @@ def data_file(scriptfile):
 
 DATA_FILE = data_file(__file__)
 
+class TextXPathGeneration(unittest.TestCase):
+    def test_field_predicate(self):
+        self.assertEqual(ls.field_predicate_xpath(),
+                         './/text:p[@text:style-name="P10"][text()]'
+                         ' and .//text:p[@text:style-name="P9"][text()]')
+
 class LXMLBaseTestClass(unittest.TestCase):
     def setUp(self):
         from lxml import etree
@@ -22,6 +28,30 @@ class TestXPathValidity(LXMLBaseTestClass):
         self.assertTrue(len(ls.product_names(self.root)))
     def test_some_product_prices_found(self):
         self.assertTrue(len(ls.product_prices(self.root)))
+
+class TestXPathFirstElement(LXMLBaseTestClass):
+    def test_product_name_of_first_draw_frame(self):
+        df = ls.draw_frames(self.root)[0]
+        self.assertEqual(ls.product_names(df)[0].text,
+                         'Updated from Speerville price list, Fery 19th, 2009 ')
+    def test_first_product_name(self):
+        self.assertEqual(ls.product_names(self.root)[0].text,
+                         'Updated from Speerville price list, Fery 19th, 2009 ')
+    def test_first_price(self):
+        self.assertEqual(ls.product_prices(self.root)[0].text,
+                         'community price rounded up to nearest nickel ')
+
+class TestXPathLastElement(LXMLBaseTestClass):
+    def test_product_name_of_last_draw_frame(self):
+        """The last draw frame doesn't have a product name.  Gotcha!"""
+        df = ls.draw_frames(self.root)[-1]
+        self.assertEqual(ls.product_names(df), [])
+    def test_last_product_name(self):
+        self.assertEqual(ls.product_names(self.root)[-1].text,
+                         'Red Quinoa')
+    def test_last_price(self):
+        self.assertEqual(ls.product_prices(self.root)[-1].text,
+                         '$8.00')
 
 class TestElementCounts(LXMLBaseTestClass):
     def test_draw_frame_count(self):
@@ -46,24 +76,18 @@ class TestElementCounts(LXMLBaseTestClass):
                          163)
     def test_all_product_name_count(self):
         """Include product name fields devoid of text in the count."""
-        self.assertEqual(len(ls.product_names(self.root, with_text=False)),
+        self.assertEqual(len(ls.product_names(self.root, with_text='either')),
                          143)
     def test_all_product_price_count(self):
         """Include product price fields devoid of text in the count."""
-        self.assertEqual(len(ls.product_prices(self.root, with_text=False)),
+        self.assertEqual(len(ls.product_prices(self.root, with_text='either')),
                          172)
 
-class TestXPathFirstElement(LXMLBaseTestClass):
-    def test_product_name_of_first_draw_frame(self):
-        df = ls.draw_frames(self.root)[0]
-        self.assertEqual(ls.product_names(df)[0].text,
-                         'Updated from Speerville price list, Fery 19th, 2009 ')
-    def test_first_product_name(self):
-        self.assertEqual(ls.product_names(self.root)[0].text,
-                         'Updated from Speerville price list, Fery 19th, 2009 ')
-    def test_first_price(self):
-        self.assertEqual(ls.product_prices(self.root)[0].text,
-                         'community price rounded up to nearest nickel ')
+    def test_drawframes_with_products_and_prices(self):
+        """All draw frame elements which have textual prices and products."""
+        dfs = ls.draw_frames_with_names_and_prices(self.root)
+        self.assertEqual(len(tuple(dfs)),
+                         104)
 
 
 if __name__ == '__main__':
